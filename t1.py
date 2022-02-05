3e80909337c183eb3f46155e4b15fcec9afd6ee4
Namya LG <namyalg@gmail.com>
2022-02-05 16:16:09 +0530

    Added py script

--- t1.py
+++ t1.py
@@ -1,1 +1,11 @@
+import random
+
+n = random.random()
+
+file1 = open("myfile.txt", "a")  # append mode
+file1.write(str((n)) + "\n")
+file1.write("\n")
+file1.close()
+
+print(n)
 print("Hello world ")
