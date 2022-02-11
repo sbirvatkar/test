1f3e6f00e5244b52e09db5b21ead66ee1695e826
Namya LG <53875297+Namyalg@users.noreply.github.com>
2022-02-11 10:49:30 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -2,6 +2,19 @@ import random
 
 n = random.random()
 
+import sys
+ 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+    
 file1 = open("myfile.txt", "a")  # append mode
 file1.write(str((n)) + "\n")
 file1.write("\n")
